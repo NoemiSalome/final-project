c0f2e4ce10b1879021404369e93197955a9ca31a
NoemiSalome <n.s.frischknecht@gmail.com>
2021-06-13 20:21:43 +0200

    react-router & routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,19 +1,22 @@
 import React from 'react'
+import { BrowserRouter, Route, Switch } from 'react-router-dom'
 
 import Home from 'pages/Home'
 import Projects from 'pages/Projects'
+import ProjectDetail from 'pages/ProjectDetail'
 import About from 'pages/About'
 import Contact from 'pages/Contact'
-import Header from 'components/general/Header'
 
 export const App = () => {
   return (
-    <div>
-      {/* <Header /> */}
-      {/* <Home /> */}
-      {/* <Projects /> */}
-      {/* <About /> */}
-      <Contact /> 
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path='/' exact />
+        <Route component={ProjectDetail} path='/projects/:slug' />
+        <Route component={Projects} path='/projects' />
+        <Route component={About} path='/about' />
+        <Route component={Contact} path='/contact' />
+      </Switch>
+    </BrowserRouter>
   )
 }
--- frontend/src/pages/About.js
+++ frontend/src/pages/About.js
@@ -1,13 +1,40 @@
 import React from 'react'
 import styled from 'styled-components/macro'
 
+import Header from 'components/general/Header'
+import profile from '../images/profile.jpg'
+
+const MainContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  height: 80vh;
+  margin: 0 10px;
+`
+const ProfilePicture = styled.img`
+  width: 35%;
+`
+
+const AboutText = styled.p`
+  font-family: 'Cormorant', serif;
+  font-size: 12px;
+  text-align: justify;
+  width: 50%
+`
+
 const About = () => {
 
-    return (
-        <div>
-            About
-        </div>
-    )
+  return (
+    <>
+      <Header />
+      <MainContainer>
+        <ProfilePicture src={profile} alt="Portrait Anina" className="ProfilePicture"/>
+        <AboutText>
+          Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet, consectetur, adipisci velit, sed
+        </AboutText>
+      </MainContainer>
+    </>
+  )
 }
 
 export default About
