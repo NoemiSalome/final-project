32241cfab2aa9989352720ff5a2fd0b6a5123c85
NoemiSalome <n.s.frischknecht@gmail.com>
2021-06-16 15:51:29 +0200

    changed dataset in client.js -> fetch works now

--- frontend/src/client.js
+++ frontend/src/client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client'
 
 export default sanityClient ({
     projectId: 'otkni7oy',
-    dataset:'production',
-    apiVersion: '2021-06-16', // use a UTC date string
+    dataset:'portfolio-anina',
+    apiVersion: '2021-06-07', // use a UTC date string
     useCdn: true
 })  
