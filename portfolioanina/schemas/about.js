e0498ed57ef0593768921d80cb3f7b11a67347fd
NoemiSalome <n.s.frischknecht@gmail.com>
2021-07-01 22:02:36 +0200

    updated schema about

--- portfolioanina/schemas/about.js
+++ portfolioanina/schemas/about.js
@@ -9,24 +9,31 @@ export default {
       type: 'string',
     },
     {
-      name: 'images',
-      title: 'Images',
-      type: 'image',
-      options: {
-        hotspot: true
-      },
-      fields: [
-        {
-          name: 'alt',
-          type: 'string',
-          title: 'Please enter a description for the image for screenreaders - thanks and cheers (no blank spaces - use (- and _ instead)',
-        }
-      ]
-    },
-    {
       name: 'aboutText',
       title: 'About',
       type: 'text'
+    },
+    {
+      name: 'images',
+      type: 'array',
+      title: 'Images',
+      of: [
+        { name: 'images',
+          type: 'image',
+          title: 'Image',
+          options: {
+            hotspot: true,
+          },
+          fields: [
+            {
+              name: 'alt',
+              type: 'string',
+              title: 'Alternative text',
+              description: 'Please enter a description for the image for screenreaders - thanks and cheers (no blank spaces - use (- and _ instead)'
+            },
+          ]
+        },
+      ],
     },
   ],
 }
